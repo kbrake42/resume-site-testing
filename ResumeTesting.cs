a18b0614de89a0e3af24808c85e2f2d4d8212963
kbrake42 <kbrake42+git@gmail.com>
2023-02-27 20:28:15 -0500

    Add files via upload

---
+++ ResumeTesting.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium;
+
+namespace resume_site_testing
+{
+    [TestFixture]
+    [Parallelizable]
+    class resumeTests
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            var firefoxOptions = new FirefoxOptions();
+            driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), firefoxOptions);
+
+            //var chromeOptions = new ChromeOptions();
+            //driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), chromeOptions);
+
+        }
+
+        [Test]
+        public void test1()
+        {
+            driver.Navigate().GoToUrl("http://www.resume.kenbrake.com");
+            Thread.Sleep(30000);
+
+
+
+
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Quit();
+        }
+
+    }
+}
